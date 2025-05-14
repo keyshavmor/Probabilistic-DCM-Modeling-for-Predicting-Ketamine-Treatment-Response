58edac72ee8b29f71099b526d221f932cd9cdb69
scleon <scleon@ethz.ch>
2025-05-14 13:30:37 +0200

    added steps to impement extractVOI

--- Scripts/run_srpbs_prepro_script.m
+++ Scripts/run_srpbs_prepro_script.m
@@ -1,9 +1,9 @@
-
 %skript to run prepro and glm for one subject
 % Path to the subject folder (must contain anat/ and func/ subfolders)
-%dataDir = 'C:\Users\leon\Programming\Julia\TNM_project\Data\Ketamine\sub-test1';
-dataDir = 'C:\Users\leon\Programming\Julia\TNM_project\Data\Ketamine\sub-MOA101\ses-b0';
+%dataDir = 'C:\Users\leon\Programming\Julia\TNM_project\Data\Ketamine\sub-MOA101
+dataDir = 'C:\Users\leon\Programming\Julia\TNM_project\git\data\ds005917-download\sub-MOA101\ses-b0';
 
 % Run the preprocessing
-srpbs_prepro_adj2_subject(dataDir, 1);  % run = 0 -> dryrun, run = 1 -> run the batch directly, run = 2 -> SPM Batch editor gui
-srpbs_glm_adj_subject(dataDir);
+srpbs_prepro_adj2_subject(dataDir, 0);  % run = 0 -> dryrun, run = 1 -> run the batch directly, run = 2 -> SPM Batch editor gui
+%srpbs_glm_adj_subject(dataDir);
+srpbs_extract_VOI_subject(dataDir);
