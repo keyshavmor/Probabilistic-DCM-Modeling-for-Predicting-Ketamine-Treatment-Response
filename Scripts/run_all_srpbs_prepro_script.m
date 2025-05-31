8abf7c26d90bd30e0372d70a007eef68f65514b2
scleon <scleon@ethz.ch>
2025-05-31 16:42:55 +0200

    run all now only uses session b0

--- Scripts/run_all_srpbs_prepro_script.m
+++ Scripts/run_all_srpbs_prepro_script.m
@@ -5,6 +5,8 @@ addpath(scriptsDir);
 
 projectRoot = fileparts(scriptsDir);
 
+
+% Base directory containing all subject/session folders
 baseDir     = fullfile(projectRoot, 'data', 'ds005917-download');
 
 disp(['Using data folder: ', baseDir]);
@@ -12,45 +14,40 @@ assert(isfolder(baseDir), 'Cannot find data folder: %s', baseDir);
 
 
 
-
-% Base directory containing all subject/session folders
-%baseDir = 'C:\Users\leon\Programming\Julia\TNM_project\Data\Ketamine';
-
 % Get list of all subject folders
 subjects = dir(fullfile(baseDir, 'sub-MOA*'));
 
 % Loop over subjects
-for i = 1:length(subjects)
+for i = 21:26  %1:length(subjects)
     subjDir = fullfile(baseDir, subjects(i).name);
     
     % Get list of all session folders within each subject folder
-    sessions = dir(fullfile(subjDir, 'ses-*'));
+    sessions = dir(fullfile(subjDir, 'ses-b0*'));
     
-    for j = 1:length(sessions)
+    for j = 0:length(sessions)
         dataDir = fullfile(subjDir, sessions(j).name);
         
         % Print current subject and session
         fprintf('Running subject %s, session %s\n', subjects(i).name, sessions(j).name);
         
-        % Call the preprocessing function
+        % Call the functions
         try
-            %srpbs_prepro_adj2_subject(dataDir, 1);  % run = 0 -> dryrun, run = 1
+            srpbs_prepro_adj2_subject(dataDir, 1);  % run = 0 -> dryrun, run = 1
             %srpbs_glm_adj_subject(dataDir);
             
             %tic toc measures the time between tic and toc
             tic
             %srpbs_extract_VOI_DMN(dataDir);
-            srpbs_extract_VOI_rsTozzi_11(dataDir);
+            %srpbs_extract_VOI_rsTozzi_11(dataDir);
             srpbs_extract_VOI_rsTozzi_15(dataDir);
             %srpbs_extract_VOI_rsTozzi_K(dataDir);
             
             %srpbs_construct_spDCM_DMN(dataDir);
             %srpbs_construct_spDCM_rsTozzi_11(dataDir);
-            %srpbs_construct_spDCM_rsTozzi_15(dataDir);
+            srpbs_construct_spDCM_rsTozzi_15(dataDir);
             %srpbs_construct_spDCM_rsTozzi_K(dataDir);
             toc
 
-    
 
         catch ME
             warning('Failed for %s %s: %s', subjects(i).name, sessions(j).name, ME.message);
